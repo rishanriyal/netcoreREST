911a50750ff5ab28a318c287635df477c34b6ed8
RisanM <risanm-i@zillione.com>
2022-07-05 20:23:40 +0530

    versioning

---
+++ TweetBook/Contracts/Ver1/ApiRoutes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetBook.Contracts.Ver1
+{
+    public static class ApiRoutes
+    {
+        public const string root = "api";
+        public const string version = "v1";
+
+        public const string Base = root + "/" + version;
+        public static class Posts
+        {
+            public const string GetAll = Base + "/posts";
+        }
+    }
+}
